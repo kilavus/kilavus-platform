8d6ded93e07e711d86159b78c292d58af3419f2b
Kerem Bozdas <krmbzds.github@gmail.com>
2013-10-31 11:23:01 +0200

    #6 Initialize vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KilavusPlatform::Application.config.secret_key_base = '9f35a3312f5b00dfca6e8b925d0fd29b626dcf143e1feee3912decb2ca96c35d398eaf0e18fa8496383e98ac4a730b2aa94a7c60cd7b76504bd4d4c9c9ce92e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KilavusPlatform::Application.config.session_store :cookie_store, key: '_kilavus-platform_session'
