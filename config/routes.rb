19aee10a8063779bc4667f89877738c29f3cd816
Kerem Bozdas <krmbzds.github@gmail.com>
2013-12-31 04:15:27 +0200

    #21 Create "User" model/views w/ Devise
    Simpler than words:
    $ rails g devise:install
    $ rails g devise:views
    $ rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 KilavusPlatform::Application.routes.draw do
+  devise_for :users
   get "platform/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
